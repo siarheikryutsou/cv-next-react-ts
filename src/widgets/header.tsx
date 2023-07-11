da54dc0232aa63fd3c940ead8e0917a3bfd9bfe7
Sergey Krivtsov <flashgangsta@gmail.com>
2023-07-11 16:12:57 +0200

    Init project base

---
+++ src/widgets/header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a>Home</a>
+                        <a>About</a>
+                        <a>CV</a>
+                        <a>Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
